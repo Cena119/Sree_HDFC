13cb9e1aece03a6bb7326954d314008ff0e3de7b
THANK YOU <THANK YOU@DESKTOP-7J8HO41>
2023-11-02 21:35:52 +0530

    version007

---
+++ src/test/java/Github/sree11.java
@@ -0,0 +1,25 @@
+package Github;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class sree11 {
+	
+	
+  @Test
+  public void function()  {
+	  
+	  
+	  
+  }
+  @BeforeTest
+  
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
